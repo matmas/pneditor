f573d81a2727bb86d47f44d36bc3aedf5ced476c
Martin Riesz <riesz.martin@gmail.com>
2013-01-17 00:39:18 +0100

    added missing legal information

--- src/org/pneditor/editor/RootPnml.java
+++ src/org/pneditor/editor/RootPnml.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2008-2010 Martin Riesz <riesz.martin at gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.pneditor.editor;
 
 import java.awt.BorderLayout;
@@ -15,7 +32,7 @@ import org.pneditor.editor.filechooser.ViptoolPnmlFileType;
 
 /**
  * This class is the main point of the application.
- * @author matmas
+ * @author Martin Riesz <riesz.martin at gmail.com>
  */
 public class RootPnml extends RootPflow implements Root, WindowListener, ListSelectionListener, SelectionChangedListener {
 
