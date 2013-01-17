162e8c0437e3f8af4821daa155f3b69c3d6d9ca6
Martin Riesz <riesz.martin@gmail.com>
2013-01-17 17:29:19 +0100

    changed boundedness message output

--- src/org/pneditor/editor/actions/algorithms/BoundednessAction.java
+++ src/org/pneditor/editor/actions/algorithms/BoundednessAction.java
@@ -61,7 +61,7 @@ public class BoundednessAction extends AbstractAction {
 		}
 		
 		if (isUnboundedness)
-			JOptionPane.showMessageDialog(root.getParentFrame(), "PetriNet is UNbounded ", "Algorithm output", JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(root.getParentFrame(), "PetriNet is NOT bounded ", "Algorithm output", JOptionPane.INFORMATION_MESSAGE);
 		else
 			JOptionPane.showMessageDialog(root.getParentFrame(), "PetriNet is bounded", "Algorithm output", JOptionPane.INFORMATION_MESSAGE);
 	}
