aabd419f7e9ea1c110039b845d61c782f36147e5
Martin Riesz <riesz.martin@gmail.com>
2014-04-30 15:00:13 +0200

    Update about dialog

--- src/org/pneditor/editor/RootPflow.java
+++ src/org/pneditor/editor/RootPflow.java
@@ -61,7 +61,7 @@ import org.pneditor.util.ListEditor;
 public class RootPflow implements Root, WindowListener, ListSelectionListener, SelectionChangedListener {
 
     private static final String APP_NAME = "PNEditor";
-    private static final String APP_VERSION = "0.64";
+    private static final String APP_VERSION = "0.70";
 
     public RootPflow(String[] args) {
         PNEditor.setRoot(this);
